54b7e1e6b7c36b006dbbee5cad0178cb18950eb1
santelli <santelli@princeton.edu>
2022-11-14 16:24:42 -0500

    bruh

--- DirectoryFileTrees/3FT/ft.c
+++ DirectoryFileTrees/3FT/ft.c
@@ -44,6 +44,7 @@ static size_t ulCount;
   Otherwise, sets *poNFurthest to NULL and returns with status:
   * CONFLICTING_PATH if the root's path is not a prefix of oPPath
   * MEMORY_ERROR if memory could not be allocated to complete request
+  * FILE_IN_PATH if path contains a file anywhere instead of a directory 
 */
 static int FT_traversePath(Path_T oPPath, Node_T *poNFurthest) {
    int iStatus;
@@ -72,8 +73,7 @@ static int FT_traversePath(Path_T oPPath, Node_T *poNFurthest) {
       return iStatus;
    }
 
-   if(Path_comparePath(Node_getPath(oNRoot), oPPrefix) ||
-   Node_getState(oNRoot) != 0) {
+   if(Path_comparePath(Node_getPath(oNRoot), oPPrefix)) {
       Path_free(oPPrefix);
       *poNFurthest = NULL;
       return CONFLICTING_PATH;
@@ -97,6 +97,9 @@ static int FT_traversePath(Path_T oPPath, Node_T *poNFurthest) {
          if(iStatus != SUCCESS) {
             *poNFurthest = NULL;
             return iStatus;
+         }
+         if (Node_getState(oNChild) != DIRECTORY) {
+            return FILE_IN_PATH;
          }
          oNCurr = oNChild;
       }
@@ -264,6 +267,8 @@ int FT_insertDir(const char *pcPath) {
 }
 
 
+
+
 boolean FT_containsDir(const char *pcPath) {
     return FALSE;
 }
@@ -275,7 +280,97 @@ int FT_rmDir(const char *pcPath) {
 
 int FT_insertFile(const char *pcPath, void *pvContents,
                   size_t ulLength) {
-    return 0;
+   int iStatus;
+   Path_T oPPath = NULL;
+   Node_T oNFirstNew = NULL;
+   Node_T oNCurr = NULL;
+   size_t ulDepth, ulIndex;
+   size_t ulNewNodes = 0;
+
+   assert(pcPath != NULL);
+
+   /* validate pcPath and generate a Path_T for it */
+   if(!bIsInitialized)
+      return INITIALIZATION_ERROR;
+
+   iStatus = Path_new(pcPath, &oPPath);
+   if(iStatus != SUCCESS)
+      return iStatus;
+
+   /* find the closest ancestor of oPPath already in the tree */
+   iStatus= FT_traversePath(oPPath, &oNCurr);
+   if(iStatus != SUCCESS)
+   {
+      Path_free(oPPath);
+      return iStatus;
+   }
+
+   /* no ancestor node found, so if root is not NULL,
+      pcPath isn't underneath root. */
+   if(oNCurr == NULL && oNRoot != NULL) {
+      Path_free(oPPath);
+      return CONFLICTING_PATH;
+   }
+
+   ulDepth = Path_getDepth(oPPath);
+   /* only possible if root is in fact NULL, in which case we do
+   not insert a file into the root */
+   if(oNCurr == NULL)
+      return CONFLICTING_PATH;
+   else {
+      ulIndex = Path_getDepth(Node_getPath(oNCurr))+1;
+
+      /* oNCurr is the node with the longest shared prefix with the
+      path that we are trying to insert */
+      if(ulIndex == ulDepth+1 && !Path_comparePath(oPPath,
+                                       Node_getPath(oNCurr))) {
+         Path_free(oPPath);
+         return ALREADY_IN_TREE;
+      }
+   }
+
+   /* starting at oNCurr, build rest of the path one level at a time */
+   while(ulIndex <= ulDepth) {
+      Path_T oPPrefix = NULL;
+      Node_T oNNewNode = NULL;
+
+      /* generate a Path_T for this level */
+      iStatus = Path_prefix(oPPath, ulIndex, &oPPrefix);
+      if(iStatus != SUCCESS) {
+         Path_free(oPPath);
+         if(oNFirstNew != NULL)
+            (void) Node_free(oNFirstNew);
+         /* assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount)); */
+         return iStatus;
+      }
+
+      /* insert the new node for this level */
+      iStatus = Node_new(oPPrefix, oNCurr, &oNNewNode);
+      if(iStatus != SUCCESS) {
+         Path_free(oPPath);
+         Path_free(oPPrefix);
+         if(oNFirstNew != NULL)
+            (void) Node_free(oNFirstNew);
+         /* assert(CheckerDT_isValid(bIsInitialized, oNRoot, ulCount)); */
+         return iStatus;
+      }
+
+      /* set up for next level */
+      Path_free(oPPrefix);
+      oNCurr = oNNewNode;
+      ulNewNodes++;
+      if(oNFirstNew == NULL)
+         oNFirstNew = oNCurr;
+      ulIndex++;
+   }
+
+   Path_free(oPPath);
+   /* update DT state variables to reflect insertion */
+   if(oNRoot == NULL)
+      oNRoot = oNFirstNew;
+   ulCount += ulNewNodes;
+
+   return SUCCESS;
 }
 
 boolean FT_containsFile(const char *pcPath) {
