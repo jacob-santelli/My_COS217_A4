b19f9925ec799d3b4c1d041387f387fcb1058ff7
joshuaqyang <joshuayang2014@gmail.com>
2022-11-14 22:09:03 -0500

    commiting before pull

--- DirectoryFileTrees/3FT/ft.c
+++ DirectoryFileTrees/3FT/ft.c
@@ -632,7 +632,7 @@ char *FT_toString(void) {
    nodes = DynArray_new(ulCount);
    (void) FT_preOrderTraversal(oNRoot, nodes, 0);
 
-   DynArray_map(nodes, (void (*)(void *, void*)) DT_strlenAccumulate,
+   DynArray_map(nodes, (void (*)(void *, void*)) FT_strlenAccumulate,
                 (void*) &totalStrlen);
 
    result = malloc(totalStrlen);
@@ -642,7 +642,7 @@ char *FT_toString(void) {
    }
    *result = '\0';
 
-   DynArray_map(nodes, (void (*)(void *, void*)) DT_strcatAccumulate,
+   DynArray_map(nodes, (void (*)(void *, void*)) FT_strcatAccumulate,
                 (void *) result);
 
    DynArray_free(nodes);
